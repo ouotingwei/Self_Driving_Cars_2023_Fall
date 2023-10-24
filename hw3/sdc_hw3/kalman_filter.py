2addf10726eb2c90488b66508385a71b8719dc60
tingweiouo <kklb1716@gmail.com>
2023-10-24 21:38:01 +0800

    hw3 ok

--- hw3/sdc_hw3/kalman_filter.py
+++ hw3/sdc_hw3/kalman_filter.py
@@ -7,23 +7,27 @@ class KalmanFilter:
         
         # Transition matrix
         self.A = np.identity(3)
-        self.B = np.identity(3)
+        self.B = np.identity(3) 
         
         # State covariance matrix
         self.S = np.identity(3) * 1
         
         # Observation matrix
-        self.C = None
+        self.C = np.array([[1, 0, 0],
+                           [0, 1, 0]])
         
         # State transition error
-        self.R = None
+        self.R = np.identity(3) 
         
         # Measurement error
-        self.Q = None
+        self.Q = np.identity(2) * 0.6
 
     def predict(self, u):
-        raise NotImplementedError
+        self.state = self.A @ self.state + self.B @ u
+        self.S = self.A @ self.S @ self.A.T + self.R
 
     def update(self, z):
-        raise NotImplementedError
-        return self.x, self.S
+        K = self.S @ self.C.T @ np.linalg.inv(self.C @ self.S @ self.C.T + self.Q)
+        self.state = self.state + K @ (z - self.C @ self.state)
+        self.S = (np.identity(3) - K @ self.C) @ self.S
+        return self.state, self.S
