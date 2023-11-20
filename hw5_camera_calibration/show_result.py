5629f671a319940b30a5abb916a2f3abe6dd4ec5
tingweiouo <kklb1716@gmail.com>
2023-11-21 02:32:46 +0800

    hw5 code ok

--- hw5_camera_calibration/show_result.py
+++ hw5_camera_calibration/show_result.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import click_utils as click
 import cv2 
 import os
+import matplotlib.animation as animation
+import time
 
 def get_transformation_matrix():
     image_path = './calibration/camera/1518069838279552217.jpg'
@@ -66,62 +68,132 @@ def get_transformation_matrix():
     return intrinsic, transformation_matrix
 
 
-def project2qrcode(intrinsic, transformation_matrix):
-    lidar_folder = './qrcode/lidar/'
-    camera_folder = './qrcode/camera/'
-    lidar_files = os.listdir(lidar_folder)
-    camera_files = os.listdir(camera_folder)
-    lidar_len = len(lidar_files)
-    cam_len = len(camera_files)
+def project2moving(intrinsic, transformation_matrix):
+    print("[!] (moving_video.mp4) is processing !")
+    output_folder = './moving_output_frames'
+    lidar_folder = './moving/lidar/'
+    camera_folder = './moving/camera/'
+    camera_files = sorted(os.listdir(camera_folder), key=lambda x: int(x.split('.')[0]))
+    lidar_files = sorted(os.listdir(lidar_folder), key=lambda x: int(x.split('.')[0]))
+    loop_len = len(lidar_files)
 
-    delta = cam_len / lidar_len
+    os.makedirs(output_folder, exist_ok=True)
 
-    print(delta)
+    for frame in range(loop_len):
+        img = cv2.imread(camera_folder + camera_files[frame])
+        pointcloud = np.load(lidar_folder + lidar_files[frame])
 
-    k = 0
-    for i in range(cam_len):
-        if i % 3 == 0:
-            i += 1
-        
-        else:
-            img = cv2.imread(camera_folder + camera_files[k])
-            pointcloud = np.load(lidar_folder + lidar_files[i])
+        uv = []
 
-            fig = plt.figure(figsize=(12.8, 7.2), dpi=100)
-            ax = fig.add_subplot()
+        projection_model = np.array([[1, 0, 0, 0],
+                                        [0, 1, 0, 0],
+                                        [0, 0, 1, 0]])
 
-            uv = []
+        for j in range(len(pointcloud)):
+            pw = np.concatenate([pointcloud[j, :3], [1]])
+            temp = intrinsic @ projection_model @ transformation_matrix @ pw
+            scales = 1 / temp[2]
+            temp = temp * scales
+            uv.append(temp)
 
-            for j in range(len(pointcloud)):
-                pw = np.concatenate([pointcloud[j, :3], [1]])
-                temp = intrinsic @ transformation_matrix[:3, :] @ pw
-                uv.append(temp)
+        uv = np.array(uv).T
 
-            uv = np.array(uv).T
+        result_file = os.path.join(output_folder, f'{frame}.png')
 
-            ax.imshow(img)
-            ax.set_xlim(0, 1280)
-            ax.set_ylim(720, 0)
-            ax.scatter(uv[0, :], uv[1, :], c=pointcloud[:, 2], marker=',', s=10, edgecolors='none', alpha=0.7, cmap='jet')
-            ax.set_axis_off() 
+        plt.figure(figsize=(12.8, 7.2), dpi=100)
+        plt.imshow(img)
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+        plt.scatter(uv[0, :], uv[1, :], c=pointcloud[:, 2], marker=',', s=10, edgecolors='none', alpha=0.7, cmap='jet')
+        plt.axis('off')
 
-            k += 1
-            plt.pause(0.1)  # Pause to allow the plot to update
+        plt.savefig(result_file)
+        plt.close()
+
+
+def project2nctu(intrinsic, transformation_matrix):
+    print("[!] (NCTU_video.mp4) is processing !")
+    output_folder = './NCTU_output_frames'
+    lidar_folder = './NCTU/lidar/'
+    camera_folder = './NCTU/camera/'
+    camera_files = sorted(os.listdir(camera_folder), key=lambda x: int(x.split('.')[0]))
+    lidar_files = sorted(os.listdir(lidar_folder), key=lambda x: int(x.split('.')[0]))
+    loop_len = len(lidar_files)
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    for frame in range(loop_len):
+        img = cv2.imread(camera_folder + camera_files[frame])
+        pointcloud = np.load(lidar_folder + lidar_files[frame])
+
+        uv = []
+
+        projection_model = np.array([[1, 0, 0, 0],
+                                        [0, 1, 0, 0],
+                                        [0, 0, 1, 0]])
+
+        for j in range(len(pointcloud)):
+            pw = np.concatenate([pointcloud[j, :3], [1]])
+            temp = intrinsic @ projection_model @ transformation_matrix @ pw
+            scales = 1 / temp[2]
+            temp = temp * scales
+            uv.append(temp)
+
+        uv = np.array(uv).T
+
+        result_file = os.path.join(output_folder, f'{frame}.png')
+
+        plt.figure(figsize=(12.8, 7.2), dpi=100)
+        plt.imshow(img)
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+        plt.scatter(uv[0, :], uv[1, :], c=pointcloud[:, 2], marker=',', s=10, edgecolors='none', alpha=0.7, cmap='jet')
+        plt.axis('off')
+
+        plt.savefig(result_file)
+        plt.close()
+
+
+def images_to_video(input_folder, output_video_path, fps=15):
+    image_files = sorted(os.listdir(input_folder), key=lambda x: int(x.split('.')[0]))
+
+    # Assuming all images have the same resolution as the first image
+    first_image = cv2.imread(os.path.join(input_folder, image_files[0]))
+    height, width, layers = first_image.shape
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+    for image_file in image_files:
+        image_path = os.path.join(input_folder, image_file)
+        img = cv2.imread(image_path)
+        video.write(img)
+
+    video.release()
+    cv2.destroyAllWindows()
 
 
 def main():
     intrinsic, transformation_matrix = get_transformation_matrix()
-    transformation_matrix = np.array([[0.85942994, 0.51122949, -0.00495944, 0.12869505],
-                                      [0.03469834, -0.0680042, -0.99708147, -0.32801881],
-                                      [-0.51007471, 0.85674958, -0.07618366, -0.13943649],
-                                      [0, 0, 0, 1]])
+    #transformation_matrix = np.array([[0.85942994, 0.51122949, -0.00495944, 0.12869505],
+    #                                  [0.03469834, -0.0680042, -0.99708147, -0.32801881],
+    #                                  [-0.51007471, 0.85674958, -0.07618366, -0.13943649],
+    #                                  [0, 0, 0, 1]])
     
-    focal_length = 698.939
-    intrinsic = np.array([[focal_length, 0, 1280/2],
-                          [0, focal_length, 720/2],
-                          [0, 0, 1]])
+    #focal_length = 698.939
+    #intrinsic = np.array([[focal_length, 0, 1280/2],
+    #                      [0, focal_length, 720/2],
+    #                      [0, 0, 1]])
     
-    #project2qrcode(intrinsic, transformation_matrix)
+    project2moving(intrinsic, transformation_matrix)
+    images_folder = './moving_output_frames'
+    output_video_path = './moving_video.mp4'
+    images_to_video(images_folder, output_video_path)
+
+    project2nctu(intrinsic, transformation_matrix)
+    images_folder = './NCTU_output_frames'
+    output_video_path = './NCTU_video.mp4'
+    images_to_video(images_folder, output_video_path)
 
 if __name__ == '__main__':
     main()
--- hw5_camera_calibration/subscribe_rosbag.py
+++ hw5_camera_calibration/subscribe_rosbag.py
@@ -7,54 +7,63 @@ from sensor_msgs.msg import CompressedImage, PointCloud2
 import sensor_msgs.point_cloud2 as pcl2
 from cv_bridge import CvBridge
 
-output_root = './qrcode/'
-output_root_lidar  = output_root + 'lidar/'
+output_root = './NCTU/'
+output_root_lidar = output_root + 'lidar/'
 output_root_camera = output_root + 'camera/'
+global file_name
+file_name = 0
 
 if not os.path.exists(output_root_lidar):
-  os.makedirs(output_root_lidar)
+    os.makedirs(output_root_lidar)
 
 if not os.path.exists(output_root_camera):
-  os.makedirs(output_root_camera)
+    os.makedirs(output_root_camera)
 
 
 def read_point_from_msg(msg):
-
-  points_list = []
-  for point in pcl2.read_points(msg, skip_nans=True, field_names=("x", "y", "z", "intensity")):
-    points_list.append(point)
-
-  return np.asarray(points_list, dtype=np.float32)
+    points_list = []
+    for point in pcl2.read_points(msg, skip_nans=True, field_names=("x", "y", "z", "intensity")):
+        points_list.append(point)
+    return np.asarray(points_list, dtype=np.float32)
 
 
 class MySubscriber():
+    def __init__(self):
+        rospy.Subscriber("/points", PointCloud2, self.lidar_callback, queue_size=None)
+        rospy.Subscriber("/left/image/compressed", CompressedImage, self.camera_callback, queue_size=None)
 
-  def __init__(self):
-    rospy.Subscriber("/points",                PointCloud2,      self.lidar_callback,  queue_size=None)
-    rospy.Subscriber("/left/image/compressed", CompressedImage,  self.camera_callback, queue_size=None)
+    def lidar_callback(self, msg):
+        global file_name
+        # Check the header
+        # print(msg.header, '\n')
 
+        timestamp = str(msg.header.stamp.secs) + "{:09d}".format(msg.header.stamp.nsecs)
+        pointcloud = read_point_from_msg(msg)
+        np.save(output_root_lidar + str(file_name) + '.npy', pointcloud)
 
-  def lidar_callback(self, msg):
+        # Call camera callback after processing lidar data
+        self.camera_callback(msg)
 
-    # Check the header
-    # print(msg.header, '\n')
+    def camera_callback(self, msg):
+        global file_name
+        # Check the header
+        # print(msg.header, '\n')
 
-    timestamp =  str(msg.header.stamp.secs) + "{:09d}".format(msg.header.stamp.nsecs)
-    pointcloud = read_point_from_msg(msg)
-    np.save(output_root_lidar + timestamp + '.npy', pointcloud)
+        timestamp = str(msg.header.stamp.secs) + "{:09d}".format(msg.header.stamp.nsecs)
+        img = CvBridge().compressed_imgmsg_to_cv2(msg)
 
+        # Check if the image is not empty and has a valid size
+        if img is not None and img.size > 0:
+            # Check the type of the image
+            print("Image type:", img.dtype)
 
-  def camera_callback(self, msg):
-
-    # Check the header
-    # print(msg.header, '\n')
-
-    timestamp =  str(msg.header.stamp.secs) + "{:09d}".format(msg.header.stamp.nsecs)
-    img = CvBridge().compressed_imgmsg_to_cv2(msg)
-    cv2.imwrite(output_root_camera + timestamp + '.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 90])
+            cv2.imwrite(output_root_camera + str(file_name) + '.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 90])
+            file_name += 1
+        else:
+            print("Warning: Empty or invalid image received.")
 
 
 if __name__ == '__main__':
-  rospy.init_node('subscriber_node', anonymous=True)
-  MySubscriber()
-  rospy.spin()
+    rospy.init_node('subscriber_node', anonymous=True)
+    MySubscriber()
+    rospy.spin()
