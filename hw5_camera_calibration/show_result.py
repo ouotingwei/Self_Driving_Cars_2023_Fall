22e26f7f47805bc84a825a7e47cedcb856d2e092
tingweiouo <kklb1716@gmail.com>
2023-11-22 00:55:18 +0800

    hw5 done

--- hw5_camera_calibration/show_result.py
+++ hw5_camera_calibration/show_result.py
@@ -6,48 +6,65 @@ import os
 import matplotlib.animation as animation
 import time
 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import click  # Assuming this is a custom module for clicking points in the image
+
 def get_transformation_matrix():
+    # Paths to the image and lidar data
     image_path = './calibration/camera/1518069838279552217.jpg'
     lidar_path = './calibration/lidar/1518069842434335859.npy'
 
+    # Read the image and lidar data
     image = cv2.imread(image_path)
     lidar = np.load(lidar_path)
 
+    # Get image dimensions
     height, width, _ = image.shape
-    print(height, width)
+    print("Image Dimensions:", height, width)
 
+    # Camera intrinsic parameters
     focal_length = 698.939
     intrinsic = np.array([[focal_length, 0, width/2],
                           [0, focal_length, height/2],
                           [0, 0, 1]])
-    
+
+    # Distortion coefficients
     dist = np.zeros(5)
 
+    # Get 2D and 3D coordinates by clicking points on the image and lidar data
     uv_coordinates = click.click_points_2D(image)
-    
     world_coordinates = click.click_points_3D(lidar)
 
+    # Solve PnP using RANSAC algorithm
     _, rvec, tvec, _ = cv2.solvePnPRansac(world_coordinates, uv_coordinates, intrinsic, dist)
 
+    # Refine the solution using Levenberg-Marquardt optimization
     rvec, tvec = cv2.solvePnPRefineLM(world_coordinates, uv_coordinates, intrinsic, dist, rvec, tvec)
 
+    # Convert rotation vector to rotation matrix
     R, _ = cv2.Rodrigues(rvec)
 
+    # Create the transformation matrix
     transformation_matrix = np.column_stack((R, tvec))
     transformation_matrix = np.vstack((transformation_matrix, [0, 0, 0, 1]))
 
     print("Transformation Matrix:")
     print(transformation_matrix)
 
+    # Visualization of the 3D lidar points projected onto the image
     fig = plt.figure(figsize=(12.8, 7.2), dpi=100)
     ax = fig.add_subplot()
 
     uv = []
 
+    # Projection model matrix
     projection_model = np.array([[1, 0, 0, 0],
                                  [0, 1, 0, 0],
                                  [0, 0, 1, 0]])
 
+    # Project lidar points onto the image
     for j in range(len(lidar)):
         pw = np.concatenate([lidar[j, :3], [1]])
         temp = ((intrinsic @ projection_model) @ transformation_matrix) @ pw.T
@@ -57,38 +74,48 @@ def get_transformation_matrix():
 
     uv = np.array(uv).T
 
+    # Plot the image with lidar points
     ax.imshow(image)
     ax.set_xlim(0, 1280)
     ax.set_ylim(720, 0)
     ax.scatter(uv[0, :], uv[1, :], c=lidar[:, 2], marker=',', s=10, edgecolors='none', alpha=0.7, cmap='jet')
     ax.set_axis_off()
 
-    plt.show()  # This line will display the plot
+    plt.show()
 
     return intrinsic, transformation_matrix
 
 
 def project2moving(intrinsic, transformation_matrix):
     print("[!] (moving_video.mp4) is processing !")
+
+    # Output folder for saving processed frames
     output_folder = './moving_output_frames'
     lidar_folder = './moving/lidar/'
     camera_folder = './moving/camera/'
+
+    # Get sorted list of camera and lidar files
     camera_files = sorted(os.listdir(camera_folder), key=lambda x: int(x.split('.')[0]))
     lidar_files = sorted(os.listdir(lidar_folder), key=lambda x: int(x.split('.')[0]))
     loop_len = len(lidar_files)
 
+    # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
+    # Loop through each frame in the video
     for frame in range(loop_len):
+        # Read the image and lidar data for the current frame
         img = cv2.imread(camera_folder + camera_files[frame])
         pointcloud = np.load(lidar_folder + lidar_files[frame])
 
         uv = []
 
+        # Projection model matrix
         projection_model = np.array([[1, 0, 0, 0],
-                                        [0, 1, 0, 0],
-                                        [0, 0, 1, 0]])
+                                     [0, 1, 0, 0],
+                                     [0, 0, 1, 0]])
 
+        # Project lidar points onto the image
         for j in range(len(pointcloud)):
             pw = np.concatenate([pointcloud[j, :3], [1]])
             temp = intrinsic @ projection_model @ transformation_matrix @ pw
@@ -98,6 +125,7 @@ def project2moving(intrinsic, transformation_matrix):
 
         uv = np.array(uv).T
 
+        # Save the result image for the current frame
         result_file = os.path.join(output_folder, f'{frame}.png')
 
         plt.figure(figsize=(12.8, 7.2), dpi=100)
