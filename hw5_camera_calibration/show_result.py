4c05246115cafbff2a4f6e27bb0d7bfa8e791a06
tingweiouo <kklb1716@gmail.com>
2023-11-23 00:38:26 +0800

    hw5 done

--- hw5_camera_calibration/show_result.py
+++ hw5_camera_calibration/show_result.py
@@ -6,11 +6,6 @@ import os
 import matplotlib.animation as animation
 import time
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import click  # Assuming this is a custom module for clicking points in the image
-
 def get_transformation_matrix():
     # Paths to the image and lidar data
     image_path = './calibration/camera/1518069838279552217.jpg'
@@ -106,7 +101,11 @@ def project2moving(intrinsic, transformation_matrix):
     for frame in range(loop_len):
         # Read the image and lidar data for the current frame
         img = cv2.imread(camera_folder + camera_files[frame])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Load lidar point cloud
         pointcloud = np.load(lidar_folder + lidar_files[frame])
+        
 
         uv = []
 
@@ -140,37 +139,57 @@ def project2moving(intrinsic, transformation_matrix):
 
 
 def project2nctu(intrinsic, transformation_matrix):
+    # Print processing message
     print("[!] (NCTU_video.mp4) is processing !")
+
+    # Set output folder and input folders for lidar and camera data
     output_folder = './NCTU_output_frames'
     lidar_folder = './NCTU/lidar/'
     camera_folder = './NCTU/camera/'
+
+    # Get sorted list of camera and lidar files
     camera_files = sorted(os.listdir(camera_folder), key=lambda x: int(x.split('.')[0]))
     lidar_files = sorted(os.listdir(lidar_folder), key=lambda x: int(x.split('.')[0]))
+
+    # Get the number of frames in the dataset
     loop_len = len(lidar_files)
 
+    # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
+    # Loop through each frame
     for frame in range(loop_len):
+        # Read the camera image
         img = cv2.imread(camera_folder + camera_files[frame])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Load lidar point cloud
         pointcloud = np.load(lidar_folder + lidar_files[frame])
 
+        # Initialize empty list to store UV coordinates
         uv = []
 
+        # Define a projection model matrix
         projection_model = np.array([[1, 0, 0, 0],
-                                        [0, 1, 0, 0],
-                                        [0, 0, 1, 0]])
+                                     [0, 1, 0, 0],
+                                     [0, 0, 1, 0]])
 
+        # Loop through each point in the point cloud
         for j in range(len(pointcloud)):
+            # Append UV coordinates to the list
             pw = np.concatenate([pointcloud[j, :3], [1]])
             temp = intrinsic @ projection_model @ transformation_matrix @ pw
             scales = 1 / temp[2]
             temp = temp * scales
             uv.append(temp)
 
+        # Convert UV coordinates to array and transpose
         uv = np.array(uv).T
 
+        # Define result file path
         result_file = os.path.join(output_folder, f'{frame}.png')
 
+        # Plot the camera image with lidar points overlaid
         plt.figure(figsize=(12.8, 7.2), dpi=100)
         plt.imshow(img)
         plt.xlim(0, 1280)
@@ -178,40 +197,38 @@ def project2nctu(intrinsic, transformation_matrix):
         plt.scatter(uv[0, :], uv[1, :], c=pointcloud[:, 2], marker=',', s=10, edgecolors='none', alpha=0.7, cmap='jet')
         plt.axis('off')
 
+        # Save the result image
         plt.savefig(result_file)
+
+        # Close the plot
         plt.close()
 
 
 def images_to_video(input_folder, output_video_path, fps=15):
+    # Get sorted list of image files in the input folder
     image_files = sorted(os.listdir(input_folder), key=lambda x: int(x.split('.')[0]))
 
-    # Assuming all images have the same resolution as the first image
+    # Read the first image to get its dimensions
     first_image = cv2.imread(os.path.join(input_folder, image_files[0]))
     height, width, layers = first_image.shape
 
+    # Define the video codec and create a VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 
+    # Loop through each image file and write it to the video
     for image_file in image_files:
         image_path = os.path.join(input_folder, image_file)
         img = cv2.imread(image_path)
         video.write(img)
 
+    # Release the VideoWriter and close all OpenCV windows
     video.release()
     cv2.destroyAllWindows()
 
 
 def main():
     intrinsic, transformation_matrix = get_transformation_matrix()
-    #transformation_matrix = np.array([[0.85942994, 0.51122949, -0.00495944, 0.12869505],
-    #                                  [0.03469834, -0.0680042, -0.99708147, -0.32801881],
-    #                                  [-0.51007471, 0.85674958, -0.07618366, -0.13943649],
-    #                                  [0, 0, 0, 1]])
-    
-    #focal_length = 698.939
-    #intrinsic = np.array([[focal_length, 0, 1280/2],
-    #                      [0, focal_length, 720/2],
-    #                      [0, 0, 1]])
     
     project2moving(intrinsic, transformation_matrix)
     images_folder = './moving_output_frames'
